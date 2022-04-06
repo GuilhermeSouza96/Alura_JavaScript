4040323d18df80994b0ced4875047402ced6c187
Guilherme Souza <souza1102@hotmail.com>
2022-04-06 01:04:31 -0300

    Exemplos da Aula 1

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, Mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = 'Meu nome é: ';
+const meuNome = "Guilherme";
+
+//concatenação (+)
+
+console.log(citacao + meuNome);
+
+//template string OU template literal
